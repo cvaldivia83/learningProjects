47bef8cf05a99813cb8cc5a287066f9681c6bc0a
Carla Valdivia <56138978+cvaldivia83@users.noreply.github.com>
2020-07-30 09:53:23 -0300

    FizzBuzz challenge
    Exercise on one common and simple interview question. 
    FizzBuzz:  log to the console Fizz if a number can be divided by 3, Buzz if number can be divided by 5 and the actual number if none of the 2 options are possible.

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+/* Fizz Buzz exercice: 
+
+Counting progressively from 1 to 100: 
+
+- if number can be divided by three, print 'Fizz';
+- if number can be divided by five, print 'Buzz';
+- else, print number. */ 
+
+for(let i = 1; i < 100; i++){
+    if(i % 3 == 0) { 
+        console.log('Fizz!');
+    } else if(i % 5 == 0) { 
+        console.log('Buzz!');
+    } else {
+        console.log(i)
+    }
+};
+
